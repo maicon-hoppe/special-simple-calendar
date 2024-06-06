e98d4744f9b892b7c7f35e764d4641d72728258b
Maicon J. Hoppe <99830251+maicon-hoppe@users.noreply.github.com>
2024-06-06 20:47:12 -0300

    month transition
    Signed-off-by: Maicon J. Hoppe <99830251+maicon-hoppe@users.noreply.github.com>

--- homepage/tests.py
+++ homepage/tests.py
@@ -17,6 +17,16 @@ class HomepageHelperFunctionsTests(SimpleTestCase):
         january_list = get_month_list(1, 2024)
         self.assertEqual(
             january_list,
-            [date(2023, 12, 31) + timedelta(days=-c) for c in range(0, 42)],
+            [date(2023, 12, 31) + timedelta(days=c) for c in range(0, 42)],
             "Lists don't match.",
         )
+
+    def test_get_month_list_invalid_month(self: Self) -> None:
+        """
+        get_month_list() returns -1 if the month is invalid
+        """
+
+        self.assertEqual(get_month_list(12000, 2024), -1, "Lists don't match.")
+        self.assertEqual(get_month_list(13, 2024), -1, "Lists don't match.")
+        self.assertEqual(get_month_list(0, 2024), -1, "Lists don't match.")
+        self.assertEqual(get_month_list(-1, 2024), -1, "Lists don't match.")
--- homepage/views.py
+++ homepage/views.py
@@ -2,7 +2,7 @@ from datetime import date, timedelta
 from typing import Any, Literal, Self
 
 from django.views.generic import TemplateView
-from django.views.generic.dates import MonthMixin
+from django.views.generic.dates import MonthMixin, YearMixin
 
 
 def get_month_list(
@@ -22,17 +22,52 @@ def get_month_list(
         return -1
 
     this_month_first = date.fromisoformat(date_string)
-    first_monday = this_month_first - timedelta(days=this_month_first.isoweekday())
+    if this_month_first.weekday() == 6:
+        first_monday = this_month_first
+    else:
+        first_monday = this_month_first - timedelta(days=this_month_first.isoweekday())
 
     return [first_monday + timedelta(days=c) for c in range(0, 42)]
 
 
 class HomepageView(MonthMixin, TemplateView):
     template_name = "homepage/index.html"
-    month = "Julho"
-    month_format = "%B"
+    month_format = "%m"
 
     def get_context_data(self: Self, **kwargs) -> dict[str, Any]:
         context = super().get_context_data(**kwargs)
+
         context["date_list"] = get_month_list(date.today().month)
+        context["today"] = {
+            "year": date.today().year,
+            "month": date.today().month,
+            "day": date.today().day,
+        }
+        context["next"] = {
+            "month": date.today().month + 1 if date.today().month < 12 else 1,
+            "year": date.today().year + 1,
+        }
+        context["previous"] = {
+            "month": date.today().month - 1 if date.today().month > 1 else 12,
+            "year": date.today().year - 1,
+        }
+
+        return context
+
+
+class MonthPageView(YearMixin, HomepageView):
+
+    def get_context_data(self, **kwargs) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+
+        context["date_list"] = get_month_list(self.get_month(), self.get_year())
+        context["next"] = {
+            "month": int(self.get_month()) + 1 if self.get_month() < 12 else 1,
+            "year": self.get_year() + 1,
+        }
+        context["previous"] = {
+            "month": self.get_month() - 1 if self.get_month() > 1 else 12,
+            "year": self.get_year() - 1,
+        }
+
         return context
