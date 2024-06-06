e24758b9558610eeb04e91dbd3969ee218e39827
Maicon J. Hoppe <99830251+maicon-hoppe@users.noreply.github.com>
2024-06-06 20:47:12 -0300

    month transition
    Signed-off-by: Maicon J. Hoppe <99830251+maicon-hoppe@users.noreply.github.com>

--- homepage/urls.py
+++ homepage/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = "homepage"
 urlpatterns = [
     path("", views.HomepageView.as_view(), name="homepage"),
+    path("<int:year>/<int:month>/", views.MonthPageView.as_view(), name="month_page"),
 ]
